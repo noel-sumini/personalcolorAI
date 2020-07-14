dc6e7bcbb0bded939aa6482ab0cb74ad6642d741
noel-sumini <ysm2820@gmail.com>
2020-07-14 23:50:04 +0900

    L eye/skin 구분하여 칼럼 생성 및 이미지 필터링

--- color_pjt/app.py
+++ color_pjt/app.py
@@ -211,7 +211,7 @@ model.summary()
 
 es = EarlyStopping(monitor='val_loss', patience=5)
 
-model.compile(loss = categorical_crossentropy, optimizer = Adam(lr=0.01), metrics = ['accuracy'])
+model.compile(loss = categorical_crossentropy, optimizer = Adam(lr=0.001), metrics = ['accuracy'])
 model.fit(input_data, output_data, batch_size = 32, epochs = 50, verbose = 1, validation_split = 0.3, callbacks = [es])
 
 
